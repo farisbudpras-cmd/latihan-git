1f85e65b74d57e0f8ab5a8f59dda3c8e2e44b165
Faris Budi Prasetyo <farisbudpras@gmail.com>
2025-12-09 20:56:45 +0700

    add hello jepang1

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Wolrd");
         System.out.println("Hello indonesia");
+        System.out.println("Hello Jepang");
+         System.out.println("Hello Jepang1");
     }
 }
